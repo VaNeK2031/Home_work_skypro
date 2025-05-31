57cf790d3fe9131fd95e68f563bb21c086542e15
VaNeK203 <dr.vanyok@gmail.com>
2025-05-31 23:09:49 +0300

    create func mask_card_account

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,35 @@
-def mask_account_card(card_or_accunt_number: str) -> str:
-    pass
+from masks import get_mask_account, get_mask_card_number
+
+def mask_account_card(data: str) -> str:
+    """
+       Возвращает строку с замаскированным номером карты или счёта.
+
+       Определяет тип финансового инструмента (карта или счёт) на основе входной строки,
+       применяет соответствующую маску к номеру и возвращает обновлённую строку.
+
+       Args:
+           data (str): Строка, содержащая тип и номер карты или счёта.
+                       Примеры:
+                           - "Visa Platinum 7000792289606361"
+                           - "Счет 73654108430135874305"
+
+       Returns:
+           str: Строка с замаскированным номером.
+                Примеры:
+                   - "Visa Platinum 7000 79** **** 6361"
+                   - "Счет **4305"
+       """
+    # Полученные данные приведем к нижнему регистру и разобъем на список, содержащий отдельно имя(имена) отдельно номер
+    parts = data.split()
+    number = parts[-1]
+    name = ' '.join(parts[:-1])
+    masked_card = ''
+    masked_account = ''
+    # Проверяем получен счет или название карты, и производим маскировку данных
+    if name == 'счет':
+        return f"{name.title()} {get_mask_account(number)}"
+    elif name != 'счет':
+        return f"{name.title()} {get_mask_card_number(number)}"
 
 
 def get_date(date: str) -> str:
@@ -7,4 +37,5 @@ def get_date(date: str) -> str:
 
 
 if __name__ == '__main__':
-    pass
+    user_input = input("Введите данные: ")
+    print(mask_account_card(user_input))
